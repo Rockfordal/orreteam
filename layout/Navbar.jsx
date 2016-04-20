c9093b0ed2df95e70098152013e910755a8f3286
Anders Lundström <anders@urkraft.se>
2016-04-20 15:40:10 +0200

    dynamsikt aktiva menylänkar

--- layout/Navbar.jsx
+++ layout/Navbar.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import {Navbar, NavItem} from 'react-materialize';
 import {Link} from 'react-router';
+import classNames from 'classnames';
 
 class MyNavbar extends React.Component {
-    render() {
-        return (
-          <div>
+  render() {
+    var loc = this.props.location.pathname;
+    return (
+      <div>
         <Navbar brand='React Materialize OrientDB' right>
-          <NavItem className="active" href='#/cars'>Bilar</NavItem>
-          <NavItem href='#/garage'>Garage</NavItem>
-          </Navbar>
-           {this.props.children}
-          </div>
-        ) } }
+          <NavItem href='#/cars'   className={classNames({active: loc=='/cars'})}>Bilar</NavItem>
+          <NavItem href='#/garage' className={classNames({active: loc== '/garage'})}>Garage</NavItem>
+        </Navbar>
+        {this.props.children}
+      </div>
+    ) } }
 
 export default MyNavbar
